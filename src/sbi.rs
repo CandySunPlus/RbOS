c82f3815bfb27a238d5cdc341a8e30c655beaca7
NikSun <candysfm@gmail.com>
2023-04-21 23:51:25 +0800

    add more

---
+++ src/sbi.rs
@@ -0,0 +1,17 @@
+#![allow(unused)]
+
+use sbi_rt::{legacy, system_reset, NoReason, Shutdown, SystemFailure};
+
+pub fn console_putchar(c: usize) {
+    #[allow(deprecated)]
+    legacy::console_putchar(c);
+}
+
+pub fn shutdown(failure: bool) -> ! {
+    if !failure {
+        system_reset(Shutdown, NoReason);
+    } else {
+        system_reset(Shutdown, SystemFailure);
+    }
+    unreachable!("It should shutdown");
+}
