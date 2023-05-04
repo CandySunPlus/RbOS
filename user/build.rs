e26aadb1b5aafa3b787a7f814644276a57525099
NikSun <candysfm@gmail.com>
2023-05-04 19:31:14 +0800

    update build script

--- user/build.rs
+++ user/build.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::fs::{read_dir, File};
-use std::io::{Read, Result, Write};
-use std::path::PathBuf;
+use std::io::{BufReader, BufWriter, Read, Result, Write};
+use std::path::Path;
 
 static BASE_ADDRESS: usize = 0x80400000;
 static STEP: usize = 0x20000;
@@ -14,40 +14,37 @@ fn main() {
 }
 
 fn set_app_link_script() -> Result<()> {
-    let dst = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let dst = Path::new(&out_dir);
 
-    let mut linker_temp = File::open(LINK_SCRIPT)?;
-
+    let mut linker_temp = BufReader::new(File::open(LINK_SCRIPT)?);
     let mut linker_temp_str = String::new();
 
     linker_temp.read_to_string(&mut linker_temp_str)?;
 
-    let mut apps = read_dir("src/bin")
-        .unwrap()
+    read_dir("src/bin")?
         .map(|dir_entry| {
-            let mut name_with_ext = dir_entry.unwrap().file_name().into_string().unwrap();
-            name_with_ext.drain(name_with_ext.find('.').unwrap()..name_with_ext.len());
+            let mut name_with_ext = dir_entry.unwrap().file_name().to_string_lossy().to_string();
+            name_with_ext.drain(name_with_ext.find('.').unwrap()..);
             name_with_ext
         })
-        .collect::<Vec<_>>();
+        .enumerate()
+        .try_for_each(|(app_id, app)| {
+            let linker_content = linker_temp_str.replace(
+                &format!("{:x}", BASE_ADDRESS),
+                &format!("{:x}", BASE_ADDRESS + STEP * app_id),
+            );
 
-    apps.sort();
+            let linker_file_path = dst.join(format!("linker_{}.ld", app));
+            let mut linker_file = BufWriter::new(File::create(&linker_file_path)?);
+            linker_file.write_all(linker_content.as_bytes())?;
 
-    for (app_id, app) in apps.iter().enumerate() {
-        let linker_content = linker_temp_str.replace(
-            format!("{:x}", BASE_ADDRESS).as_str(),
-            format!("{:x}", BASE_ADDRESS + STEP * app_id).as_str(),
-        );
+            println!(
+                "cargo:rustc-link-arg-bin={app}=-T{}",
+                linker_file_path.to_str().unwrap(),
+                app = app
+            );
 
-        let linker_file_path = dst.join(format!("linker_{app}.ld"));
-        let mut linker_file = File::create(&linker_file_path)?;
-        linker_file.write_all(linker_content.as_bytes())?;
-
-        println!(
-            "cargo:rustc-link-arg-bin={app}=-T{}",
-            linker_file_path.to_str().unwrap()
-        );
-    }
-
-    Ok(())
+            Ok(())
+        })
 }
