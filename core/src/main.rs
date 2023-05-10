97084322e30ed2ea2cb9358b540b5f90de327d51
NikSun <candysfm@gmail.com>
2023-05-10 20:58:47 +0800

    update

--- core/src/main.rs
+++ core/src/main.rs
@@ -94,7 +94,7 @@ pub fn rust_main() -> ! {
     info!("[kernel] back to world!");
 
     trap::init();
-    loader::load_apps();
+    // loader::load_apps();
     trap::enable_timer_interrupt();
     timer::set_next_trigger();
     task::run_first_task();
